a7999f5a2dafafcf74f8ac9eabdef2c127beb607
Jay Chae <ejchae08@gmail.com>
2013-02-15 23:03:25 -0600

    Added basic companies layout.

--- techstack/urls.py
+++ techstack/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from techstack_app.views import *
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,4 +11,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'techstack.views.home', name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^companies/', companies_page),
+    url(r'', home_page),
 )
+
+
--- techstack_app/views.py
+++ techstack_app/views.py
@@ -1,1 +1,27 @@
 # Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import Context, RequestContext
+from django.template.loader import get_template
+from django.shortcuts import render_to_response
+
+from techstack_app.models import *
+
+
+def home_page(request):
+    """docstring for home_page"""
+    companies = Company.objects.all()
+    variables = RequestContext(request, {
+        'companies': companies
+    })
+
+    return render_to_response('base.html', variables)
+
+
+def companies_page(request):
+    # companies = Company.objects.order_by('-company_name')[:20]
+    companies = Company.objects.all()
+    variables = RequestContext(request, {
+        'companies': companies
+    })
+
+    return render_to_response('companies.html', variables)
